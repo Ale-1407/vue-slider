e4e45e6a41bba4bb49c526727488d536fc1329c7
Ale-1407 <alessiacarrera07@gmail.com>
2022-11-11 17:24:39 +0100

    inserimento immagini in array

--- js/main.js
+++ js/main.js
@@ -13,3 +13,16 @@ da parte nostra
 
 */
 
+const app = new Vue ({
+    el: '#root',
+    data: {
+        titolo: 'Amsterdam',
+        title: 'text-center',
+        obj: [
+            { img: 'https://unsplash.com/photos/QRtym77B6xk/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id: 1 },
+            { img: 'https://unsplash.com/photos/2Hs8zbwOLDA/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8M3x8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id: 2 },
+            { img: 'https://unsplash.com/photos/t2hgHV1R7_g/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Nnx8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id:  3},
+            { img: 'https://unsplash.com/photos/-e4vLFZV9QM/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8OHx8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id: 4 }
+        ]
+    }
+})
