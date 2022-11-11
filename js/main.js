5b21e299545aa35f776047905e9da1998075dadc
Ale-1407 <alessiacarrera07@gmail.com>
2022-11-11 18:20:26 +0100

    evento al click su icone

--- js/main.js
+++ js/main.js
@@ -21,8 +21,26 @@ const app = new Vue ({
         obj: [
             { img: 'https://unsplash.com/photos/QRtym77B6xk/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id: 1 },
             { img: 'https://unsplash.com/photos/2Hs8zbwOLDA/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8M3x8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id: 2 },
-            { img: 'https://unsplash.com/photos/t2hgHV1R7_g/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Nnx8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id:  3},
+            { img: 'https://unsplash.com/photos/t2hgHV1R7_g/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Nnx8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id: 3 },
             { img: 'https://unsplash.com/photos/-e4vLFZV9QM/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8OHx8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id: 4 }
-        ]
+        ],
+        indice: 0,
+    },
+    methods: {
+        cambioDestra: function(){
+            if( this.indice > this.obj.lenght){
+             this.indice = 0;
+           } else {
+            this.indice++;
+           }
+        },
+        cambioSinistra: function(){
+            if( this.indice > this.obj.lenght){
+                this.indice = 0;
+              } else {
+                this.indice--;
+               }
+        }
+
     }
 })
