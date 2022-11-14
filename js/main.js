0102a4c5a64e55da92951b48357b60fbe3682f58
Ale-1407 <alessiacarrera07@gmail.com>
2022-11-14 09:50:50 +0100

    modifica url immagini

--- js/main.js
+++ js/main.js
@@ -13,16 +13,16 @@ da parte nostra
 
 */
 
-const app = new Vue ({
+var app = new Vue ({
     el: '#root',
     data: {
         titolo: 'Amsterdam',
         title: 'text-center',
         obj: [
-            { img: 'https://unsplash.com/photos/QRtym77B6xk/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id: 1 },
-            { img: 'https://unsplash.com/photos/2Hs8zbwOLDA/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8M3x8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id: 2 },
-            { img: 'https://unsplash.com/photos/t2hgHV1R7_g/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Nnx8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id: 3 },
-            { img: 'https://unsplash.com/photos/-e4vLFZV9QM/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8OHx8YW1zdGVyZGFtfGVufDB8fHx8MTY2ODE4MDMwNA&force=true&w=640', id: 4 }
+            { img: '/img/img-1.jpg', id: 1 },
+            { img: '/img/img-2.jpg', id: 2 },
+            { img: '/img/img-3.jpg', id: 3 },
+            { img: '/img/img-4.jpg', id: 4 }
         ],
         indice: 0,
     },
@@ -35,7 +35,7 @@ const app = new Vue ({
            }
         },
         cambioSinistra: function(){
-            if( this.indice > this.obj.lenght){
+            if( this.indice < this.obj.lenght){
                 this.indice = 0;
               } else {
                 this.indice--;
